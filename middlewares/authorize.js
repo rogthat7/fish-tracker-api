1d1f5cfaa374558dbc6bba4c187692841e23993c
rogthat7 <rogthat7@gmail.com>
2021-05-08 23:30:56 +0530

    check for header added

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -1,6 +1,9 @@
 const jwt = require('jsonwebtoken');
 
 exports.Authorized = function (req, res, next) {
+    if (!req.headers.authorization) {
+        res.status(401).send("Header Missing");
+    }
     let token = req.headers.authorization.split(' ')[1];
     if (!token) res.status(401).send("No Token found");
 
